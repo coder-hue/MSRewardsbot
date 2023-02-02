cf0a4319b245ca33d60dd183c29937992cf755d4
Coder-Hue <61210314+coder-hue@users.noreply.github.com>
2023-02-02 10:51:19 -0600

    Manifest V3 Test 1

---
+++ src/backgroundv3.js
@@ -0,0 +1,14 @@
+try {
+    importScripts("status/DailyQuest.js",
+    "status/DailyRewardStatus.js",
+    "quest/searchQuest.js",
+    "quest/quizDailyQuest.js",
+    "googleTrend.js",
+    "utility.js",
+    "badge.js",
+    "exception.js",
+    "background.js",
+    "test.js");
+  } catch (e) {
+    console.error(e);
+  }
